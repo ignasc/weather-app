df25de9c241a92597ea1e981b717a41ab65fd392
ignasc <ignui.c@gmail.com>
2024-04-11 20:25:29 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
+import './css/reset.css';
+import AppTitle from "./components/AppTitle";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <AppTitle title="Your local weather" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
